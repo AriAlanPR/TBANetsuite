353a6da86cb98f32ad5038424733dc1967912c9d
Ari Alan Pérez Romero <shaymin2003@gmail.com>
2021-06-28 11:24:01 -0500

    Update tba.js

--- model/tba.js
+++ model/tba.js
@@ -10,8 +10,8 @@ let timestamp = date.getTime();
 //generated for example netsuite instance case
 var organization = '1234567';
 
-const Consumer_key = '62d71c684cd3cc4f7fab4a77e4dcd6ee776991724b6c06eb60b3eacb575bf78d';
-const Consumer_secret = 'b5cd0820e4d98005366bc9e58025eefff7a3cef81fa611af8b542eeaac06aae0';
+const Consumer_key = '';
+const Consumer_secret = '';
 const http_Method = 'POST';
 let base_Uri = () => {
     return `https://${organization}.restlets.api.netsuite.com/rest/requesttoken`;
@@ -22,7 +22,7 @@ const encode_signature = () => {
         http_method: http_Method,
         base_uri: encodeURIComponent(base_Uri()),
         rp_to_normalize: { //request parameters to normalize
-            oauth_callback: 'https://7db307784bd4.ngrok.io',
+            oauth_callback: 'https://...',
             oauth_consumer_key: Consumer_key,
             oauth_nonce: nonce,
             oauth_signature_method: 'HMAC-SHA256',
